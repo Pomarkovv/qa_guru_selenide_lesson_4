c5364f14af74cb224201bf5420f9766b3436b812
Pomarkov Ivan <ivan.e.pomarkov@gmail.com>
2025-02-18 20:17:21 +0700

    add api tests

--- src/test/java/web/components/CalendarComponent.java
+++ src/test/java/web/components/CalendarComponent.java
@@ -8,5 +8,17 @@ public class CalendarComponent {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + day).click();
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
